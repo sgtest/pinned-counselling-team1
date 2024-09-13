2b0d1252a58285a56eef259fbeeb93eab0df77c8
yonggyo1125 <yonggyo00@kakao.com>
2024-09-13 19:36:39 +0900

    .

--- counseling-service/src/main/java/xyz/sangdam/counseling/services/CounselingInfoService.java
+++ counseling-service/src/main/java/xyz/sangdam/counseling/services/CounselingInfoService.java
@@ -4,16 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xyz.sangdam.counseling.controllers.CounselingSearch;
 import xyz.sangdam.counseling.entities.Counseling;
+import xyz.sangdam.counseling.exceptions.CounselingNotFoundException;
 import xyz.sangdam.counseling.repositories.CounselingRepository;
+import xyz.sangdam.file.services.FileInfoService;
 import xyz.sangdam.global.ListData;
 
 @Service
 @RequiredArgsConstructor
 public class CounselingInfoService {
-    private final CounselingRepository groupRepository;
+    private final CounselingRepository repository;
+    private final FileInfoService fileInfoService;
 
     public Counseling get(Long cNo) {
-        return null;
+        Counseling item = repository.findById(cNo).orElseThrow(CounselingNotFoundException::new);
+
+        // 추가 처리
+
+        addInfo(item);
+
+        return item;
     }
 
     public ListData<Counseling> getList(CounselingSearch search) {
--- counseling-service/src/main/java/xyz/sangdam/counseling/services/CounselingSaveService.java
+++ counseling-service/src/main/java/xyz/sangdam/counseling/services/CounselingSaveService.java
@@ -6,6 +6,7 @@ import xyz.sangdam.counseling.controllers.RequestCounseling;
 import xyz.sangdam.counseling.entities.Counseling;
 import xyz.sangdam.counseling.exceptions.CounselingNotFoundException;
 import xyz.sangdam.counseling.repositories.CounselingRepository;
+import xyz.sangdam.file.services.FileUploadDoneService;
 
 import java.util.Objects;
 
@@ -13,6 +14,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class CounselingSaveService {
     private final CounselingRepository repository;
+    private final FileUploadDoneService uploadDoneService;
 
     public void save(RequestCounseling form) {  // 상담 등록
         Long cNo = form.getCNo();
@@ -38,5 +40,7 @@ public class CounselingSaveService {
         counseling.setCounselingLimit(form.getCounselingLimit());
 
         repository.saveAndFlush(counseling);
+
+        uploadDoneService.process(form.getGid()); // 파일 업로드 완료 처리
     }
 }
