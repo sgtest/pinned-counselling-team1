72057653052628e086e3bddca2dd8370bee021f8
yonggyo1125 <yonggyo00@kakao.com>
2024-09-13 19:22:40 +0900

    .

--- counseling-service/src/main/java/xyz/sangdam/counseling/controllers/RequestReservation.java
+++ counseling-service/src/main/java/xyz/sangdam/counseling/controllers/RequestReservation.java
@@ -1,7 +1,6 @@
 package xyz.sangdam.counseling.controllers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Lob;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,8 +8,11 @@ import java.time.LocalTime;
 
 @Data
 public class RequestReservation {
-    private Long cNo;
+    private Long cNo; // 집단 상담 프로그램 번호
 
+    private String category; // 개인 상담일 경우 상담 분류
+
+    // 개인 상담 필수
     @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate rDate; // 예약일
 
@@ -18,6 +20,6 @@ public class RequestReservation {
     private LocalTime rTime; // 예약시간
 
     /* 개인상담 */
-    @Lob
     private String reason; // 상담 사유
+
 }
--- counseling-service/src/main/java/xyz/sangdam/counseling/services/CounselingSaveService.java
+++ counseling-service/src/main/java/xyz/sangdam/counseling/services/CounselingSaveService.java
@@ -3,7 +3,11 @@ package xyz.sangdam.counseling.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xyz.sangdam.counseling.controllers.RequestCounseling;
+import xyz.sangdam.counseling.entities.Counseling;
+import xyz.sangdam.counseling.exceptions.CounselingNotFoundException;
 import xyz.sangdam.counseling.repositories.CounselingRepository;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +15,28 @@ public class CounselingSaveService {
     private final CounselingRepository repository;
 
     public void save(RequestCounseling form) {  // 상담 등록
+        Long cNo = form.getCNo();
+        String mode = Objects.requireNonNullElse(form.getMode(), "write");
+
+        Counseling counseling = null;
+        if (mode.equals("update") && cNo != null) {
+            counseling = repository.findById(cNo).orElseThrow(CounselingNotFoundException::new);
+        } else {
+            counseling = new Counseling();
+            counseling.setGid(form.getGid());
+        }
+
+        counseling.setCounselingDes(form.getCounselingDes());
+        counseling.setCounselingName(form.getCounselingName());
+        counseling.setCounselorName(form.getCounselorName());
+        counseling.setCounselorEmail(form.getCounselorEmail());
+
+        counseling.setReservationSdate(form.getReservationSdate());
+        counseling.setReservationEdate(form.getReservationEdate());
+
+        counseling.setCounselingDate(form.getCounselingDate());
+        counseling.setCounselingLimit(form.getCounselingLimit());
+
+        repository.saveAndFlush(counseling);
     }
 }
