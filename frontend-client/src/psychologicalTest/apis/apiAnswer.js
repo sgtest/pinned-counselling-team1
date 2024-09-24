9e9afdab838e0a046b80daf5e0614b81a96d389f
yonggyo1125 <yonggyo00@kakao.com>
2024-09-24 14:32:37 +0900

    .

--- frontend-client/src/psychologicalTest/apis/apiAnswer.js
+++ frontend-client/src/psychologicalTest/apis/apiAnswer.js
@@ -1,4 +1,9 @@
 import saveProcess from '@/commons/libs/saveProcess';
+import requestData from '@/commons/libs/requestData';
+
 export default function answer(form) {
   return saveProcess(`/psychologicalTest/answer`, 'POST', form);
 }
+
+export const getAnswer = (resultId) =>
+  requestData(`/psychologicalTest/answer/${resultId}`);
--- frontend-client/src/psychologicalTest/containers/AnswerContainer.js
+++ frontend-client/src/psychologicalTest/containers/AnswerContainer.js
@@ -1,9 +1,23 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AnswerView from '../components/AnswerView';
+import { getAnswer } from '../apis/apiAnswer';
 
 const AnswerContainer = ({ params }) => {
   const { resultId } = params;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getAnswer(resultId);
+        setData(data);
+        console.log('data', data);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [resultId]);
 
   return <AnswerView />;
 };
